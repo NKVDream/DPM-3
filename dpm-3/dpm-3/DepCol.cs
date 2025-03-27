a955c8c67e8f4bca29967a299034fadffaa4ae19
NKVDream <Kmv235@yandex.ru>
2025-03-27 05:55:24 -0700

    1 done

---
+++ dpm-3/dpm-3/DepCol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class DepCul
+{
+    int Months { get; set; }
+    int Deposit { get; set; }
+    public DepCul(int Months, int Deposite)
+    {
+        this.Months = Months;
+        this.Deposit = Deposite;
+    }
+    public void PreDepProcent()
+    {
+        for (int i = 0; i > Months; i++)
+        {
+            Deposit += Deposit/100 * 7;
+        }
+        Console.WriteLine(Deposit);
+    }
+    public void PostDepProcent()
+    {
+        int i = 0;
+        while (i < Months)
+        {
+            Deposit += Deposit/100 * 7;
+            i++;
+        }
+        Console.WriteLine(Deposit);
+        
+    }
+}
---
+++ dpm-3/dpm-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Task1
+{
+    static void Main()
+    {
+        Console.WriteLine("deposit amount:");int deposite = int.Parse(Console.ReadLine());
+        Console.WriteLine("number of months:");int months = int.Parse(Console.ReadLine());
+        DepCul output = new DepCul(months, deposite);
+        output.PostDepProcent();
+        output.PreDepProcent();
+    }
+}
