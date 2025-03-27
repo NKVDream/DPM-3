53c1a07e0f4659b817a42dcd8778842cd3c91fff
NKVDream <Kmv235@yandex.ru>
2025-03-27 06:28:56 -0700

    2, 3

---
+++ dpm-3/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Task2
+{
+    static void Main()
+    {
+        Console.WriteLine("your name:");string name = Console.ReadLine();
+        Console.WriteLine("your lastname: ");string surename = Console.ReadLine();
+        Task2.Out(name, surename);
+    }
+    static void Out(string name, string surename)
+    {
+        string info = name + surename;
+        foreach(char c in info)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ dpm-3/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Task3
+{
+    static void Main()
+    {
+        Console.WriteLine();int num = int.Parse(Console.ReadLine());
+        Task3.PosCount(num);
+        Task3.PreCount(num);
+    }
+    static void PosCount(int num)
+    {
+        int count = 0;
+        do
+        {
+            num = num / 10;
+            count++;
+        }
+        while (num > 0);
+        Console.WriteLine(count);
+    }
+    static void PreCount (int num)
+    {
+        int count = 0;
+        if (num == 0)
+        {
+            count = 1;
+        }
+        while (num > 0)
+        {
+            num /= 10;
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+}
