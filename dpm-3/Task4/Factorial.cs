177db8f6341b10f848e91987f6a7f1bc7312ca87
NKVDream <Kmv235@yandex.ru>
2025-03-28 05:55:50 -0700

    fourth task is done

---
+++ dpm-3/Task4/Factorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Factorial
+{
+    public double n {  get; set; }
+    public Factorial(double n)
+    {
+        this.n = n;
+    }
+    public void Nfac(double n)
+    {
+        double fac = 1;
+        if (n == 0)
+            Console.WriteLine(1);
+        else
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                fac *= i;
+            }
+        }
+        Console.WriteLine(fac);
+
+    }
+}
---
+++ dpm-3/Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Task4
+{
+    static void Main()
+    {
+        Console.WriteLine("factorial=");double n = double.Parse(Console.ReadLine());
+        Factorial fac = new Factorial(n);
+        fac.Nfac(n);
+    }
+}
